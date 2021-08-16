dd613097aa9b11d3c1e9c2c9c3e1e4ecf7cc17e5
lucasgandace <lucasgandac@hotmail.com>
2021-08-16 18:44:46 -0300

    Implemeting GUI

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
+
+import Game
+import Logic
+import Rendering
+
+window = InWindow "Jogo da Velha - Trabalho de Paradigmas" (640, 480) (100, 100)
+backgroundColor = makeColor 0 255 191 255
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
