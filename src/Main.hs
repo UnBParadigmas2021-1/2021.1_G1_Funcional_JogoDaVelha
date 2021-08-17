e4ee1356a97055bb3ac87049bc5cd77a93839a97
Wictor Girardi <wictor.girardi@gmail.com>
2021-08-16 22:06:18 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Logic
 import Rendering
 
 window = InWindow "Functional" (screenWidth, screenHeight) (100, 100)
-backgroundColor = makeColor 0 255 191 255
+backgroundColor = makeColor 0 0 0 255
 
 main :: IO ()
 main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
